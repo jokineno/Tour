79cd4311926ffeb9bf926db6054ac5daee56cc01
Olli Jokinen <olli.jokinen@helsinki.fi>
2019-03-22 16:26:42 +0200

    requirements

--- application/__init__.py
+++ application/__init__.py
@@ -1,28 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
-
-# Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
 
 import os
 
-
 if os.environ.get("HEROKU"):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-
 else:
-# Käytetään tasks.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
-# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
-# samassa paikassa
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
-# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"    
     app.config["SQLALCHEMY_ECHO"] = True
 
-# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
+  
 db = SQLAlchemy(app)
 
-# Luetaan kansiosta application tiedoston views sisältö
 from application import views
 
 from application.tasks import models
@@ -32,7 +23,7 @@ from application.auth import models
 from application.auth import views
 
 
-# kirjautuminen
+# login
 from application.auth.models import User
 from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
@@ -49,8 +40,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-# Luodaan lopulta tarvittavat tietokantataulut
-try:
+try: 
     db.create_all()
 except:
     pass
